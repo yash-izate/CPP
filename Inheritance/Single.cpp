b6b672e3a7f0f8f1868c8c9ce0a5053cbf2bd35d
Yash Izate <yashizate.softech@gmail.com>
2025-04-16 14:12:55 +0530

    update

--- Inheritance/Single.cpp
+++ Inheritance/Single.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 class Base {
 public:
-    void show() {
+    void showB() {
         cout << "This is the Base class" << endl;
     }
 };
 
 class Derived : public Base {
     public:
-        void show() {
+        void showD() {
             cout << "This is the Derived class" << endl;
         }
 };
 
 int main() {
     Derived obj;
-    obj.show();
+    obj.showD();
+    obj.showB();
     return 0;
 }
