3992e56c26ee3ec2fd915a581baaaad4ac39be3b
Yash Izate <yashizate.softech@gmail.com>
2025-10-11 01:07:10 +0530

    classes and objects

---
+++ tut8.cpp
@@ -0,0 +1,74 @@
+// Member Functions - check binary
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Binary
+{
+private:
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+
+void Binary ::read(void)
+{
+    cout << "Enter a binary number: " << endl;
+    cin >> s;
+}
+
+void Binary ::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "Incorrect Binary Format." << endl;
+            exit(0);
+        }
+    }
+    cout << "Correct Binary Format." << endl;
+}
+
+void Binary ::ones_compliment(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void Binary ::display(void)
+{
+    cout << "Displaying One's Compliment of Binary Number: " << endl;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+}
+
+int main()
+{
+    Binary b;
+
+    b.read();
+    b.chk_bin();
+    b.ones_compliment();
+    b.display();
+
+    return 0;
+}
