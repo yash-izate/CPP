9b470cd391b7cb372b0ec41abe5a43c8998a2b1d
Yash Izate <yashizate.softech@gmail.com>
2025-12-04 00:13:51 +0530

    added some theory in tut1

--- Tutorials/tut1.cpp
+++ Tutorials/tut1.cpp
@@ -1,7 +1,52 @@
+// Comment - single line (//)  & Multi Line (/*  */)
+
+/* Variable - It is a container to hold data.
+
+Scope - Scope of a variable is the region in code where the existance of variable is valid.
+Based On Scope Variable can be classified into two types :-
+
+1. Local Variable - Local variables are declared inside the braces of any function and can be accessed only from there.
+2. Global Variable - Global variables are declared outside any function and can be accessed from anywhere.
+
+Notes:- Local and  Global variable can have same name in C++.
+*/
+
+/*
+Data Types -   Data types define the type of data a variable can hold.
+               Data types in C++ are categorized in three groups:  1. Built-in.  2. User-defined  3. Derived
+
+              // Built in Data types in C++
+                int    =   integer numbers
+                float  =   decimal numbers of low precision
+                char   =   character  'a' , '@',  '&'
+                double =   decimal numbers of high precision
+                bool   =  true or false
+
+             // User Defined Data Types in C++
+             struct = Used to group different data types under one name.
+             class  = similar to struct but supports data hiding, member functions and OOPs concepts.
+             union  = Stores different datatype but only one at a time.
+             typedef / using   = Creates a new name (alias) for an existing data type.
+             enum = Set of named integer constant.
+             template =  allow creation of generic data type.
+
+             // Derived Data types in C++
+             Arrays = a collection of elements of the same data type stored in contiguous memory
+             Pointers =  Stores memory address of another variable
+             References (&x) = an alias for an existing variable (nickmname).
+
+*/
+
+/* Basic Input and Output in C++
+            cout <<    Insertion operator
+            cin  >>    Extraction Operator
+*/
+
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
     int num;
 
