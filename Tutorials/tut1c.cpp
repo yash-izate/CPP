742c347eb388c58ee32385c6dae2fe91abe5e885
Yash Izate <yashizate.softech@gmail.com>
2025-12-06 15:23:31 +0530

    updated

--- Tutorials/tut1c.cpp
+++ Tutorials/tut1c.cpp
@@ -2,6 +2,10 @@
 Definition - A pointer is a variable that stores the memory address of another variable.
 */
 
+//   & - (address of) operator
+
+//  * - (value at) - dereference operator
+
 #include <iostream>
 using namespace std;
 
@@ -13,6 +17,8 @@ int main()
     cout << "Value of a: " << a << endl;
     cout << "Address of a: " << &a << endl;
     cout << "Pointer ptr: " << ptr << endl;
+
+    // using deference operator - gives which value is present at that address
     cout << "Value using pointer: " << *ptr << endl;
 
     return 0;
