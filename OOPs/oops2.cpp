60a0425d0ec18e7b0df6a9380ba345ce4b95e1e7
Yash Izate <yashizate.softech@gmail.com>
2025-12-08 02:39:59 +0530

    directory6 changed

--- OOPs/oops2.cpp
+++ OOPs/oops2.cpp
@@ -25,16 +25,33 @@ public:
         cin >> totalMarks;
     }
 
-    void display() {
+    void display()
+    {
         cout << "\nName: " << name;
         cout << "\nRoll No: " << rollNo;
         cout << "\nTotal Marks: " << totalMarks << endl;
     }
-
 };
 
 int main()
 {
+    Student s[3];
+    int topperIdx = 0;
+
+    cout << "Enter Details of three Student: " << endl;
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "--------------Student " << i + 1 << "-----------------" << endl;
+        s[i].setData();
+
+        if (s[i].totalMarks > s[topperIdx].totalMarks)
+        {
+            topperIdx = i;
+        }
+    }
+
+    cout << "\n----------Topper Details---------" << endl;
+    s[topperIdx].display();
 
     return 0;
 }
