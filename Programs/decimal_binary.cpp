13d17e453882859f9b59ceefcf605989640d3810
Yash Izate <yashizate.softech@gmail.com>
2025-12-08 01:05:54 +0530

    bin

--- Programs/decimal_binary.cpp
+++ Programs/decimal_binary.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
-    int rem = 0, temp = 0, binary = 0;
-
     cout << "Enter Decimal Number: ";
     cin >> n;
 
-    // Convert to reversed binary
-    while (n > 0)
-    {
-        rem = n % 2;             // Extract bit
-        temp = temp * 10 + rem;  // Store reversed
-        n /= 2;                  // Reduce n
+    int temp = n;
+    int rev = 0;        // store reversed binary
+    int binary = 0;     // final correct binary
+
+    // Step 1: Convert decimal to reversed binary
+    while (temp > 0) {
+        int rem = temp % 2;       // remainder gives bit
+        rev = rev * 10 + rem;     // build reversed binary
+        temp = temp / 2;          // reduce number
     }
 
-    cout << "Reversed: " << temp << endl;
-
-    // Reverse again to get proper binary
-    while (temp > 0)
-    {
-        int digit = temp % 10;
+    // Step 2: Reverse again to get correct binary
+    while (rev > 0) {
+        int digit = rev % 10;
         binary = binary * 10 + digit;
-        temp /= 10;
+        rev = rev / 10;
     }
 
-    cout << "Binary Number: " << binary << endl;
+    cout << "Binary Number = " << binary;
 
     return 0;
 }
