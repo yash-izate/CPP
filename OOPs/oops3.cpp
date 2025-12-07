52572f487c024247fab1f9e01c60454fbc5d6c58
Yash Izate <yashizate.softech@gmail.com>
2025-12-08 03:16:39 +0530

    add

--- OOPs/oops3.cpp
+++ OOPs/oops3.cpp
@@ -1,11 +1,55 @@
-/* Program 
-*/ 
+/* Write a C++ program of operator overloading
+to add two complex numbers using addition operators (+).
+*/
 
 #include <iostream>
 using namespace std;
 
-int main() 
+class Complex
 {
- 
+private:
+    float real;
+    float imag;
+
+public:
+    Complex()
+    {
+        real = 0;
+        imag = 0;
+    }
+
+    Complex(float x, float y)
+    {
+        real = x;
+        imag = y;
+    }
+
+    void display();
+
+    Complex operator+(Complex obj);
+};
+
+Complex Complex::operator+(Complex obj)
+{
+    Complex temp;
+    temp.real = real + obj.real;
+    temp.imag = imag + obj.imag;
+    return temp;
+}
+
+void Complex ::display()
+{
+    cout << real << " + " << imag << "i." << endl;
+}
+
+int main()
+{
+    Complex c1(5, 7), c2(6, 9), c3;
+
+    c3 = c1.operator+(c2);
+
+    cout << "Result Complex: " << endl;
+    c3.display();
+
     return 0;
 }
